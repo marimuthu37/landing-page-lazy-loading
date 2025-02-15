bf4a282d1cb6f051eb4a82d2b52b0c4ee5a0b2f2
marimuthu37 <marimuthug037@gmail.com>
2025-02-15 12:02:49 +0530

    lazy-loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,25 @@
 import React, { lazy, Suspense } from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Layout from './Layout'; 
 import About from './About';
-import Places from './Places';
 import Home from './Home';
 import TravelForm from './TravelForm';
 
 const LazyPlaces = lazy(() => import('./Places'));
 
-const appRouter = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />, 
-    children: [
-      {path: '/', element: <Home />},
-      { path: 'about', element: <About /> }, 
-      { path: 'places', element: <Suspense fallback={<div>Loading...</div>}><LazyPlaces /></Suspense>},
-      {path: 'book',element:<TravelForm />},
-      {path: '*', element: <h1>Not Found</h1>}
-    ]
-  }
-]);
-
 function App() {
-  return <RouterProvider router={appRouter} />;
+  return (
+    <HashRouter>
+      <Layout />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/places" element={<Suspense fallback={<div>Loading...</div>}><LazyPlaces /></Suspense>} />
+        <Route path="/book" element={<TravelForm />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
-
