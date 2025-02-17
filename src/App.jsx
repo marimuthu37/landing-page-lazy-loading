902eb6be53f8f19896e35f17f2a3818890181054
marimuthu37 <marimuthug037@gmail.com>
2025-02-17 08:59:40 +0530

    lazy loading to all page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import React, { lazy, Suspense } from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Layout from './Layout'; 
-import About from './About';
-import Home from './Home';
-import TravelForm from './TravelForm';
+
 
 const LazyPlaces = lazy(() => import('./Places'));
+const LazyAbout = lazy(() => import('./About'));
+const LazyTravelForm = lazy(() => import('./TravelForm'));
+const LazyHome = lazy(() => import('./Home'));
+
 
 function App() {
   return (
     <HashRouter>
       <Layout />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/places" element={<Suspense fallback={<div>Loading...</div>}><LazyPlaces /></Suspense>} />
-        <Route path="/book" element={<TravelForm />} />
+        <Route path="/" element={<Suspense fallback={<h1>Loading...</h1>}><LazyHome/></Suspense>} />
+        <Route path="/about" element={<Suspense fallback={<h1>Loading...</h1>}><LazyAbout/></Suspense>} />
+        <Route path="/places" element={<Suspense fallback={<h1>Loading...</h1>}><LazyPlaces /></Suspense>} />
+        <Route path="/book" element={<Suspense fallback={<h1>Loading...</h1>}><LazyTravelForm/></Suspense>} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
     </HashRouter>
